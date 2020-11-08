68ac8e8fe25f4713686273050dcbb4138e311dbc
Peter Renaud <peterrenaud94@gmail.com>
2020-11-08 13:12:14 -0500

    added documentation

--- Politics.java
+++ Politics.java
@@ -1,16 +1,21 @@
 import java.util.*;
 import java.io.*;
+import java.net.*;
 
 class Politics {
-  String fileType;
   public Politics() {
 
   }
-
+  /**
+   * Decides based on the year which path or url to send to the parser to display the corresponding
+   * data in a JTable
+   *
+   * @param year; the year of the election results data that is to be displayed
+   * @return void
+   */
   public void getElectionResults(int year) {
     CSVParser parser = new CSVParser();
     // only working with the one year until it is working properly
     parser.parseFile("./results-2018.csv");
-
   }
 }

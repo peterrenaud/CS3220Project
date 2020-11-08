0580320fb24610ae6e72523e13307b0d173c2fed
Peter Renaud <peterrenaud94@gmail.com>
2020-11-07 21:09:14 -0500

    restructed politics class, removed unecessary ones

--- CSVParser.java
+++ CSVParser.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.*;
 
 class CSVParser {
   String line = "";
@@ -9,16 +11,35 @@ class CSVParser {
 
   }
   public void parseFile(String fileName) {
-    try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-      while((line = br.readLine()) != null) {
-        String[] data = line.split(",");
-        for (String n: data) {
-          System.out.println(n);
-        }
-      }
-      br.close();
+    int numStrings = 0;
+    String line = "";
+    List<String> lines = new ArrayList<String>();
+    try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+    while((line = br.readLine()) != null) {
+      lines.add(line);
+      numStrings++;
+      //        String data = line.split(",");
+      //        for (String n: data) {
+      //  System.out.print(n);
+      //}
+      //        System.out.println(line);
+      // System.out.println();
+    }
+    String[] columnNames = lines.remove(0).split(",");
+    String[][] data = new String[lines.size()][columnNames.length];
+
+    for (int i = 0; i<numStrings-1; i++) {
+      data[i] = lines.remove(0).split(",");
+    }
+
+    OutputTable output = new OutputTable(data, columnNames);
     } catch (IOException e) {
       System.out.print(e);
     }
+
+
+    return;
+
   }
+
 }
--- managementTool.java
+++ managementTool.java
@@ -3,12 +3,22 @@ import java.io.*;
 class managementTool {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Would you like to view election results or Elected official information?\n");
-    System.out.print("Enter (politics)\n");
-    String input = sc.nextLine();
-
+    //    System.out.print("Would you like to view election results or Elected official information?\n");
+    //    System.out.print("Enter (politics)\n");
+    //String input = sc.nextLine();
+    String input = "politics";
     if(input.equals("politics")) {
-      Politics politics = new Politics(input);
+      Politics politics = new Politics();
+      System.out.print("Would you like to view election results or Elected official information?\n");
+      //      input = sc.nextLine();
+      input = "results"; // filler until the other section is added
+      if (input.equals("results")) {
+        System.out.print("which year would you like to view?");
+        int number = 10; // filler until later years are added
+        politics.getElectionResults(number);
+      } else {
+        System.out.println("not implemented");
+      }
     } else {
       System.out.print("Input was incorrect\n");
     }

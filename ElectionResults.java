9f8e162bda905390abca92337904894f2660d2fc
Peter Renaud <peterrenaud94@gmail.com>
2020-11-13 22:03:18 -0500

    smaller file

--- ElectionResults.java
+++ ElectionResults.java
@@ -2,8 +2,9 @@ import java.io.*;
 import java.util.*;
 import javax.swing.*;
 
-// this class can be used later should other methods require the ability to perform arithatic on the data
-class ElectionResults{
+// this class can be used later should other methods require the ability to perform arithatic on the
+// data
+class ElectionResults {
   private int year;
   private int ward;
   private int poll;
@@ -12,22 +13,26 @@ class ElectionResults{
   private String candidate;
   private int totalVotes;
   private String[] data;
-  public ElectionResults() {
 
-  }
+  public ElectionResults() {}
+
   public ElectionResults(int year) {
     this.year = year;
     //    getInformation(year);
   }
+
   public int getWard() {
     return ward;
   }
+
   public int getPoll() {
     return poll;
   }
+
   public String getPollName() {
     return pollName;
   }
+
   public String getOfficeTitle() {
     return officeTitle;
   }
@@ -35,12 +40,8 @@ class ElectionResults{
   public String getName() {
     return candidate;
   }
+
   public int getTotalVotes() {
     return totalVotes;
   }
-
-
-
-
-
 }
